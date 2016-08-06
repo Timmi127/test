bdf0995178dea318b707640fa1df97e7f7ac02d6
Timmi127 <timmi127@yandex.ru>
2016-08-06 05:53:24 -0400

    Update Findjobs.java
    Сделал код более читабильным, в данном случае убрал лишние пробелы.

--- untitled2/src/main/java/Findjobs.java
+++ untitled2/src/main/java/Findjobs.java
@@ -1,34 +1,26 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
 import java.io.*;
 
-/**
- * Created by User on 15.07.2016.
- */
 public class Findjobs {
+    
+    
     int num;
     public void Find(String text,WebDriver driver) {
         num = driver.findElements(By.linkText(text)).size();
-
-
         System.out.println("Стив Джобс упоминается: " + num);
-
         File file = new File("E://Apple.txt");
 
         try {
-
             if (!file.exists()) {
                 file.createNewFile();
             }
-
+            
             PrintWriter out = new PrintWriter(file.getAbsoluteFile());
 
             try {
-
                 out.print("Steve Jobs mention " + num + "time");
             } finally {
-
                 out.close();
             }
         } catch (IOException e) {
